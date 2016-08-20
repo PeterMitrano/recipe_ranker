cf0e24e9530f45919634920a760414b002f425ab
Peter Mitrano <mitranopeter@gmail.com>
2016-08-20 00:23:16 -0400

    fixed key

--- recipe_ranker/ranker.py
+++ recipe_ranker/ranker.py
@@ -6,7 +6,7 @@ def search(keywords, recipes):
     """ recipes is an array of recipe dicts, returns a list of recipes that match """
     matching_items = []
     for item in recipes:
-        recipe_name = item['name']
+        recipe_name = item['Title']
         ratio = fuzz.partial_token_set_ratio(keywords, recipe_name)
         if ratio > GOOD_ENOUGH_RATIO:
             matching_items.append(item)
